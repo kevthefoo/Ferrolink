2aaaf7f49d70169a2447e169f6e66ab7b4081db4
Kevin Foo <kevthefoo@gmail.com>
2026-01-25 18:50:09 +1000

    feat: integrate Google Analytics and update dependencies

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -66,6 +67,7 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-B9VNSHRRRE" />
     </html>
   );
 }
